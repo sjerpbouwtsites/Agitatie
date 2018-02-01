d32e7a6d300b00685dd708df29435e3c7fdeaf96
Sjerp van Wouden <info@sjerpbouwtsites.nl>
2018-02-01 14:57:00 +0100

    head afgesplitst van header

--- header.php
+++ header.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-<link href="<?=THEME_URI?>/lettertypen/materialdesignicons.css" media="all" rel="stylesheet" type="text/css" />
-<?php wp_head(); ?>
-<?php get_template_part('sja/header/google-fonts'); ?>
-<meta name="format-detection" content="telephone=no"/>
-</head>
+<?php get_template_part('head');?>
 
 <body <?php body_class(); ?>>
 <header id='stek-kop'>
