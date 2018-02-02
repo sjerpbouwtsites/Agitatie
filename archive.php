29c2cba286de6815828fc463a7ab7a2cc55d2457
Sjerp van Wouden <info@sjerpbouwtsites.nl>
2018-02-02 13:23:56 +0100

    liz meuk er uit + bugfixes

--- archive.php
+++ archive.php
@@ -3,41 +3,31 @@
 get_header();
 
 define('POST_TYPE_NAAM', $wp_query->query['post_type']);
+$pt_mv = $wp_query->queried_object->label;
 
-set_query_var('klassen_bij_primary', "menu bg-warm");
+echo "<script>console.dir(".json_encode($pt_mv).")</script>";
+
+set_query_var('klassen_bij_primary', "archief archief-".POST_TYPE_NAAM);
 get_template_part('/sja/open-main');
 
 $pt_arch_link = get_post_type_archive_link(POST_TYPE_NAAM);
 
-//meervoud van huidige post type @ models
-$pt_mv = post_type_mv_model();
-
 //bv category -> mediterraans @ models
 $tax_waarde = gezocht_naar_tax_waarde_model();
-$archief_titel = $pt_mv . ($tax_waarde !== '' ? "<span>$tax_waarde</span>" : "");
 
-//ACF option pagina
-$pag_intro = get_field(POST_TYPE_NAAM.'_pagina_intro', 'option');
-$promo_t = get_field(POST_TYPE_NAAM.'_promotie_titel', 'option');
-$promo_p = get_field(POST_TYPE_NAAM.'_promotie', 'option');
+$archief_titel = $pt_mv . ($tax_waarde !== '' ? "<span>$tax_waarde</span>" : "");
 
 echo "<div class='verpakking'>";
 echo "<h1>$archief_titel</h1>";
 
-echo apply_filters('the_content', $pag_intro);
-
-echo "<section>";
-echo "<h2>$promo_t</h2>";
-//maakt post type objs aan en print @ controllers
-archief_generiek_loop($promo_p, 'hele-breedte');
-echo "</section>";
-
+//@TODO STANDAARD TEKST NEERKNALLEN
+archief_intro_ctrl();
 
 //generiek taxonomy blok @ klassen
 $tax_blok = new Tax_blok(array(
 	'post'		=> $post,
 	'titel'		=> $pt_mv,
-	'basis'		=> $pt_arch_link,
+	'basis'		=> $pt_arch_link . '?',
 ));
 $tax_blok->print();
 
