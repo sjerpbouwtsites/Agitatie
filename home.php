aefc4bbe9baf46f800f76995274944e732339b12
Sjerp van Wouden <info@sjerpbouwtsites.nl>
2018-02-02 18:13:31 +0100

    home init

--- home.php
+++ home.php
@@ -1,10 +1,33 @@
 <?php
 
 get_header();
-set_query_var('klassen_bij_primary', "index");
+set_query_var('klassen_bij_primary', "home verpakking");
 get_template_part('/sja/open-main');
 
-get_template_part('/sja/ga-weg');
+
+echo "<div class='marginveld'>";
+
+echo "<h1>Nieuws</h1>";
+
+
+if (have_posts()) : while (have_posts()) : the_post();
+
+	if (!isset($a)) {
+		$a = new Article_c(array(
+			'class' 	=> 'blok in-lijst',
+			'htype'		=> 2,
+			'exc_lim'	=> 140
+		), $post);
+	} else {
+		$a->art = $post;
+	}
+
+
+	$a->print();
+
+endwhile; endif;
+
+echo "</div>";
 
 get_template_part('/sja/sluit-main');
 get_footer();
