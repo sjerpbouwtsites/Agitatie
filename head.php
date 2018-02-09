66d27c082132d2d731fe6166921d4ebdd963a64e
Sjerp van Wouden <info@sjerpbouwtsites.nl>
2018-02-09 13:41:49 +0100

    google analytics template

--- head.php
+++ head.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+	<?php get_template_part('sja/header/google-analytics'); ?>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
