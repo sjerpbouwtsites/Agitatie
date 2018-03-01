17846a958843d1b8ee8515b23702b2d97234409e
Sjerp van Wouden <info@sjerpbouwtsites.nl>
2018-03-01 14:05:24 +0100

    begin aanpassing posttypes tax kan meer arg aan

--- front-page.php
+++ front-page.php
@@ -5,7 +5,7 @@ get_header();
 set_query_var('klassen_bij_primary', "voorpagina marginveld");
 get_template_part('/sja/open-main');
 
-voorpagina_voor_tekst_hook();
+do_action('voorpagina_voor_tekst_hook');
 
 tekstveld_ctrl(array(
 	'formaat'		=> 'klein',
@@ -13,7 +13,7 @@ tekstveld_ctrl(array(
 	'titel_el'		=> 'h1'
 ));
 
-voorpagina_na_tekst_hook();
+do_action('voorpagina_na_tekst_hook');
 
 get_template_part('/sja/sluit-main');
 get_footer();
